031741b3431b67ed3541a187fbeca019a7675016
Charl <94119475+Charl1980@users.noreply.github.com>
2022-08-10 11:46:23 +0200

    Update test files

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,5 +1,4 @@
 const { google } = require("googleapis");
-const { resultingClientExists } = require("workbox-core/_private");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 
