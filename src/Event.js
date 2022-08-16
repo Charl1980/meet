409a1a3015817e43f36cbacf2ec389b80645ceb7
Charl <94119475+Charl1980@users.noreply.github.com>
2022-08-16 14:06:28 +0200

    Create Event component

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    show: true,
+    hide: true
+  }
+
+  showDetailsClicked = () => {
+    this.setState({
+      show: this.state.show
+    });
+  }
+
+  hideDetailsClicked = () => {
+    this.setState({
+      hide: this.state.hide
+    });
+  }
+
   render() {
-    return <div></div>
+    const { event } = this.props;
+    return (
+      <div className='event'>
+        <h1 className='event-title'></h1>
+        <p className='event-info'></p>
+        <button className='show-event-details' onClick={() => this.showDetailsClicked()}>Show details</button>
+        <p className='event-details'></p>
+        <button className='hide-event-details' onClick={() => this.hideDetailsClicked()}>Hide details</button>
+      </div>
+    )
   }
 }
 
