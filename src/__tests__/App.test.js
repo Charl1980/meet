cbdcb1667bf9f5d433bc3f0126b2d6745c00781f
Charl <94119475+Charl1980@users.noreply.github.com>
2022-08-16 14:07:19 +0200

    Add NumberOfEvents test component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
 describe('<App /> component', () => {
 
@@ -17,6 +18,10 @@ describe('<App /> component', () => {
 
   test('Render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+
+  test('Render NumberOfEvents', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 
 });
