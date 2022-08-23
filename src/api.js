0250f938e6f845c8f2aea31c134e9faf78000855
Charl <94119475+Charl1980@users.noreply.github.com>
2022-08-23 21:25:02 +0200

    Import axios into api.js file

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
 import { mockData } from './mock-data';
+import axios from 'axios';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
+};
+
+export const getEvents = async () => {
+  return mockData;
 };
