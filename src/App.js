1d1507281f1a8bcbfbea1647692c4aacd33baecd
Charl <94119475+Charl1980@users.noreply.github.com>
2022-08-26 12:15:46 +0200

    Update the updateEvents function

--- src/App.js
+++ src/App.js
@@ -10,19 +10,49 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
+    currentLocation: 'all',
     numberOfEvents: 32
   }
 
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all')
-        ?
-        events :
-        events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length;
+      const city = location.split(' ').shift();
+      return { city, number };
     });
+    return data;
+  }
+
+  updateEvents = (location, eventCount) => {
+    const { currentLocation, numberOfEvents } = this.state;
+    if (location) {
+      getEvents().then((response) => {
+        const locationEvents =
+          location === 'all'
+            ? response.events
+            : response.events.filter((event) => event.location === location);
+        const events = locationEvents.slice(0, numberOfEvents);
+        return this.setState({
+          events: events,
+          currentLocation: location,
+          locations: response.locations
+        });
+      });
+    } else {
+      getEvents().then((response) => {
+        const locationEvents =
+          currentLocation === 'all'
+            ? response.events
+            : response.events.filter((event) => event.location === currentLocation);
+        const events = locationEvents.slice(0, eventCount);
+        return this.setState({
+          events: events,
+          numberOfEvents: eventCount,
+          locations: response.locations
+        });
+      });
+    }
   }
 
   componentDidMount() {
