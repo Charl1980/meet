350c8b6382796a7221697c636a35acebe5f4e70c
Charl <94119475+Charl1980@users.noreply.github.com>
2022-08-26 12:29:16 +0200

    Update api.js file

--- src/api.js
+++ src/api.js
@@ -31,7 +31,7 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-export const getEvents = async () => {
+export const getEvents = async (max_results = 32) => {
   NProgress.start();
   if (window.location.href.startsWith('http://localhost')) {
     NProgress.done();
