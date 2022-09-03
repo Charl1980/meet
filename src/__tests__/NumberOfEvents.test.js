dad43bd7f26caeef31441c338e3fbc75b70c1c36
Charl <94119475+Charl1980@users.noreply.github.com>
2022-09-03 08:45:26 +0200

    Update NumberOfEvents.test.js file

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -12,17 +12,6 @@ describe('<NumberOfEvents /> component', () => {
     expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
   });
 
-  test('render text input correctly', () => {
-    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
-    expect(NumberOfEventsWrapper.find('#numberOfEvents__input').prop('value')).toBe(numberOfEvents);
-  });
-
-  test('change state when input changes', () => {
-    const eventObject = { target: { value: 32 } };
-    NumberOfEventsWrapper.find('#numberOfEvents__input').simulate('change', eventObject);
-    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
-  });
-
   test('show number of events input label', () => {
     expect(NumberOfEventsWrapper.find('.numberOfEvents label')).toHaveLength(1);
   });
