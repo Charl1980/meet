e5eca64450ed3c16cc01e30644a34e0fc36fe709
Charl <94119475+Charl1980@users.noreply.github.com>
2022-09-05 00:26:42 +0200

    Changed const data to let data on line 35

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
 
   getData = () => {
     const { locations, events } = this.state;
-    const data = locations.map((location) => {
+    let data = locations.map((location) => {
       const number = events.filter((event) => event.location === location).length;
       const city = location.split(' ').shift();
       return { city, number };
