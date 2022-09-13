cc41df413b9f2be0bbb34d3be53a1dab7603447a
Charl <94119475+Charl1980@users.noreply.github.com>
2022-09-13 12:01:55 +0200

    Add a warningAlert

--- src/Alert.js
+++ src/Alert.js
@@ -42,4 +42,12 @@ class ErrorAlert extends Alert {
   }
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'orange';
+    this.fontSize = '12px';
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
