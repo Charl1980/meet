645ae1a60eed443ca224624a351408be359ef4d7
Charl <94119475+Charl1980@users.noreply.github.com>
2022-09-14 13:50:38 +0200

    Add WarningAlert to EventsList

--- src/EventList.js
+++ src/EventList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Event from './Event';
+import { WarningAlert } from './Alert';
 
-const EventList = ({ events }) => {
+const EventList = ({ events, warningText }) => {
   if (!events) return null;
   return (
     <ul className="EventList">
+      {!navigator.onLine ? (<WarningAlert text={warningText} />) : ('')}
       {events.map((event) => (
         <li key={event.id}>
           <Event event={event} />
