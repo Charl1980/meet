c2044787c6180c346fdaeec501b21d55e610e3bf
Charl <94119475+Charl1980@users.noreply.github.com>
2022-09-21 14:19:29 +0200

    Add getData function

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
-  const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 }
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(() => getData());
+  }, [events]);
+
+  const getData = () => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const data = genres.map((genre) => {
+      const value = events.filter(({ summary }) =>
+        summary.split(' ').includes(genre)).length;
+      return { name: genre, value };
+    });
+    return data;
+  };
+
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#85D2DB'];
 
   return (
     <ResponsiveContainer height={400} >
@@ -22,6 +33,10 @@ const EventGenre = ({ events }) => {
           dataKey="value"
           label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
         >
+          {
+            data.map((entry, index) =>
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} name={entry.name} />)
+          }
         </Pie>
       </PieChart>
     </ResponsiveContainer>
